a4e5448e1e5cb65a3fa9b3ee654f071e194f0a7e
Holger Pirk <hlgr@Veloceraptor.local>
2020-12-12 13:53:52 +0000

    some refactoring (plus result printing)

--- Source/Utilities.hpp
+++ Source/Utilities.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "BOSS.hpp"
 #include "Expression.hpp"
+#include <ostream>
 
 namespace boss::utilities {
 class ExpressionBuilder {
@@ -28,3 +29,24 @@ template <class... Fs> struct overload : Fs... {
 template <class... Ts> overload(Ts&&...) -> overload<std::remove_reference_t<Ts>...>;
 
 } // namespace boss::utilities
+
+static std::ostream& operator<<(std::ostream& out, boss::Expression::ReturnType const& thing) {
+  std::visit(boss::utilities::overload(
+                 [&](boss::Expression const& e) {
+                   out << e.getHead() << "[";
+                   if(!e.getArguments().empty()) {
+                     out << e.getArguments().front();
+                     for(auto it = next(e.getArguments().begin()); it != e.getArguments().end();
+                         ++it) {
+                       out << "," << *it;
+                     }
+                   }
+                   out << "]";
+                 },
+                 [&](int value) { out << value; },
+                 [&](boss::Expression::Symbol const& value) { out << value.getName(); },
+                 [&](float value) { out << value; },
+                 [&](std::string const& value) { out << "\"" << value << "\""; }),
+             thing);
+  return out;
+}
--- Tests/BOSSTests.cpp
+++ Tests/BOSSTests.cpp
@@ -8,19 +8,21 @@ using std::string;
 using boss::utilities::operator""_;
 
 TEMPLATE_TEST_CASE("Simpletons", "", boss::engines::wolfram::Engine) { // NOLINT
-  static auto engine = TestType();
+  static auto eval = [e = TestType()](boss::Expression const& expression) mutable {
+    return e.evaluate(expression);
+  };
+
   SECTION("Basics") {
-    REQUIRE(get<int>(engine.evaluate("Plus"_(5, 4))) == 9);
-    REQUIRE(get<int>(engine.evaluate("Plus"_(5, 2, 2))) == 9);
-    REQUIRE(get<int>(engine.evaluate("Plus"_(5, 2, 2))) == 9);
-    REQUIRE(get<int>(engine.evaluate("Plus"_("Plus"_(2, 3), 2, 2))) == 9);
-    REQUIRE(get<int>(engine.evaluate("Plus"_("Plus"_(3, 2), 2, 2))) == 9);
-    REQUIRE(get<string>(engine.evaluate("StringJoin"_("howdie", " ", "world"))) == "howdie world");
-    REQUIRE(get<bool>(engine.evaluate("Greater"_(5, 2))));
-    REQUIRE(!get<bool>(engine.evaluate("Greater"_(2, 5))));
-    REQUIRE(get<boss::Expression::Symbol>(engine.evaluate("Symbol"_("x"))).getName() == "x");
-    REQUIRE(get<boss::Expression>(engine.evaluate("UndefinedFunction"_(9))).getHead() ==
-            "UndefinedFunction");
+    REQUIRE(get<int>(eval("Plus"_(5, 4))) == 9);
+    REQUIRE(get<int>(eval("Plus"_(5, 2, 2))) == 9);
+    REQUIRE(get<int>(eval("Plus"_(5, 2, 2))) == 9);
+    REQUIRE(get<int>(eval("Plus"_("Plus"_(2, 3), 2, 2))) == 9);
+    REQUIRE(get<int>(eval("Plus"_("Plus"_(3, 2), 2, 2))) == 9);
+    REQUIRE(get<string>(eval("StringJoin"_("howdie", " ", "world"))) == "howdie world");
+    REQUIRE(get<bool>(eval("Greater"_(5, 2))));
+    REQUIRE(!get<bool>(eval("Greater"_(2, 5))));
+    REQUIRE(get<boss::Expression::Symbol>(eval("Symbol"_("x"))).getName() == "x");
+    REQUIRE(get<boss::Expression>(eval("UndefinedFunction"_(9))).getHead() == "UndefinedFunction");
   }
 }
 #endif // WSINTERFACE
