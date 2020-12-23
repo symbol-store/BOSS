b05c2b16ef1fda8c50c21baf34b6106c77ddcacd
Holger Pirk <hlgr@Veloceraptor.local>
2020-12-23 10:56:46 +0000

    fixed linting error

--- Source/Expression.cpp
+++ Source/Expression.cpp
@@ -1,11 +1,12 @@
 #include "Expression.hpp"
 #include "Utilities.hpp"
-bool operator==(boss::Expression const& r1, boss::Expression const& r2) {
+using namespace boss;
+bool operator==(Expression const& r1, Expression const& r2) {
   if(r1.index() == r2.index()) {
     return std::visit(
-        boss::utilities::overload(
-            [&](boss::ComplexExpression const& r1) {
-              auto const& r2Expression = std::get<boss::ComplexExpression>(r2);
+        utilities::overload(
+            [&](ComplexExpression const& r1) {
+              auto const& r2Expression = std::get<ComplexExpression>(r2);
               if(r1.getHead() != r2Expression.getHead() ||
                  r1.getArguments().size() != r2Expression.getArguments().size()) {
                 return false;
@@ -17,7 +18,7 @@ bool operator==(boss::Expression const& r1, boss::Expression const& r2) {
               }
               return true;
             },
-            [&](boss::Symbol r1) { return r1.getName() == std::get<decltype(r1)>(r2).getName(); },
+            [&](Symbol const& r1) { return r1.getName() == std::get<Symbol>(r2).getName(); },
             [&](auto r1) { return r1 == std::get<decltype(r1)>(r2); }),
         r1);
   }
