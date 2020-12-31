1b58beecb0d30811e0d14a877e9e778ba2732ee7
Hubert Mohr-Daurat <10770115+hmohrdaurat@users.noreply.github.com>
2020-12-31 13:43:28 +0000

    add python test file

---
+++ Tests/TestPython.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.insert(0, './build')
+
+import faulthandler;
+faulthandler.enable()
+
+import BOSS
+from BOSS import evaluate
+from BOSS import ComplexExpression as Expr
+from BOSS import Symbol
+
+import BOSSHelpers
+
+def TestExpressions():
+    # comment out to switch between Wolfram/Bulk
+    BOSS.setEngine(BOSS.WolframEngine)
+    BOSS.setEngine(BOSS.BulkEngine)
+
+    print(Symbol("bla"))
+    print(Expr("bla2", []).getHead())
+
+    expr = Expr("bla2", ["arg1", "arg2"])
+    print(expr)
+
+    expr2 = Expr("bla3", [1, True, 'blabla', Symbol("symb")])
+    args = expr2.getArguments()
+    print(args)
+
+    print(Symbol("A") + Symbol("B"))
+    print((expr + expr2).evaluate())
+
+    print(Expr("bla2", ["arg1", "arg2"]) + Expr("bla3", [1, True, 'blabla', Symbol("symb")]))
+
+    print(Expr("Plus", [1, 2]).evaluate())
+
+    exprToEval = Expr("Plus", [Symbol("A"), Expr("Plus", [1,2,3,4,5,6,7,8,9,10])])
+    print(exprToEval)
+
+    print(exprToEval.evaluate())
+    print(evaluate(1))
+    print(evaluate("Test!"))
+
+TestExpressions()
