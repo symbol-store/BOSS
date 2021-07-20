e0ed4836e28a246c14d96f4770d4af9d424d35e9
kandreak <andrea.piermarteri@gmail.com>
2021-07-20 04:00:49 -0700

    mosquitto integration with json parsing

--- Source/Engines/Wolfram.cpp
+++ Source/Engines/Wolfram.cpp
@@ -14,6 +14,9 @@
 #include <tuple>
 #include <vector>
 
+#include <mosquitto.h>
+#include <nlohmann/json.hpp>
+
 #define STRINGIFY(x) #x        // NOLINT
 #define STRING(x) STRINGIFY(x) // NOLINT
 
@@ -463,15 +466,123 @@ struct EngineImplementation {
 };
 
 extern "C" {
+
+// for convenience
+using json = nlohmann::json;
+std::vector < json > jsonVector;
+
 void thingy(EngineImplementation& engine) {
-  static std::thread first([&engine]() {
-    engine.evaluate("CreateTable"_("BatteryData"_, "Time"_, "Value"_, "Value2"_));
-    while(true) {
-      static auto i = 0;
-      engine.evaluate("InsertInto"_("BatteryData"_, 123189, "Status", i++));
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-  });
+
+  //K: boss table creation
+  engine.evaluate("CreateTable"_("BatteryData"_, "Time"_, "Data"_, "Position"_));
+  
+  int i;
+	//char *host = "localhost";
+    char *host = "test.mosquitto.org";
+	int port = 1883;
+	int keepalive = 60;
+	bool clean_session = true;
+	struct mosquitto *mosq = NULL;
+
+	mosquitto_lib_init();
+	mosq = mosquitto_new(NULL, clean_session, NULL);
+	if(!mosq){
+		fprintf(stderr, "Error: Out of memory.\n");
+		return 1;
+	}
+    //K: log messages not needed now
+	//mosquitto_log_callback_set(mosq, my_log_callback);
+	mosquitto_connect_callback_set(mosq, my_connect_callback);
+	mosquitto_message_callback_set(mosq, my_message_callback);
+	mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
+
+	if(mosquitto_connect(mosq, host, port, keepalive)){
+		fprintf(stderr, "Unable to connect.\n");
+		return 1;
+	}
+
+	mosquitto_loop_forever(mosq, -1, 1);
+
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+	return 0;
+
+  // OLD
+  // static std::thread first([&engine]() {
+  //   engine.evaluate("CreateTable"_("BatteryData"_, "Time"_, "Value"_, "Value2"_));
+  //   while(true) {
+  //     static auto i = 0;
+  //     engine.evaluate("InsertInto"_("BatteryData"_, 123189, "Status", i++));
+  //     std::this_thread::sleep_for(std::chrono::seconds(1));
+  //   }
+  // });
+}
+
+void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
+{
+	json data;
+    //K: TODO things with message
+	if(message->payloadlen){
+		//printf("%s %s\n", message->topic, message->payload);
+
+		//K: json stuff
+		std::string str = (char*)message->payload;
+		data = json::parse(str);
+
+		jsonVector.push_back(data);
+
+		std::cout << "\n==========\n";
+		std::cout << "message received on topic " << message->topic << "\n";
+		//std::cout << "vector size: " << jsonVector.size() << "\n";
+		std::cout << "\n";
+		std::cout << "id: " << data.at("id") << "\n";
+		std::cout << "groupId: " << data.at("groupId") << "\n";
+    std::cout << "timestamp: " << data.at("ts") << "\n";
+		std::cout << "\n";
+		std::cout << "data: " << data.at("data");
+		std::cout << "\n==========\n";
+
+    //K: boss insert
+    //K: TODO: is engine accessible? How can we make it so?
+    engine.evaluate("InsertInto"_("BatteryData"_, data.at("ts"), data.at("data"), jsonVector.size()));
+		
+		// printf(" id is: %s", data.at("id"));
+	}else{
+		printf("%s (null)\n", message->topic);
+	}
+	fflush(stdout);
+}
+
+void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
+{
+	int i;
+	if(!result){
+		/* Subscribe to broker information topics on successful connect. */
+		//mosquitto_subscribe(mosq, NULL, "$SYS/#", 2);
+
+        //K: double subscribe does the trick!
+        mosquitto_subscribe(mosq, NULL, "mosquittoTest/testTopic1", 2);
+        mosquitto_subscribe(mosq, NULL, "mosquittoTest/testTopic2", 2);
+	}else{
+		fprintf(stderr, "Connect failed\n");
+	}
+}
+
+void my_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos)
+{
+	int i;
+
+	printf("Subscribed (mid: %d): %d", mid, granted_qos[0]);
+	for(i=1; i<qos_count; i++){
+		printf(", %d", granted_qos[i]);
+	}
+	printf("\n");
+}
+
+void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str)
+{
+	/* Pring all log messages regardless of level. */
+	printf("%s\n", str);
 }
 }
 
