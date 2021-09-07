929817ad32ca0101b163481a4c522dfadf7bd583
Holger <holger@pirk.name>
2021-09-07 10:40:00 +0100

    version 2 of the operator interface

--- Source/Engines/Bulk.hpp
+++ Source/Engines/Bulk.hpp
@@ -3,14 +3,12 @@
 #include <algorithm>
 #include <map>
 #include <memory>
+#include <string>
 #include <utility>
 #include <variant>
 
 namespace boss::engines::bulk {
-using namespace std;
 class Engine : public boss::Engine {
-private:
-
 public:
   class Op {
   public:
@@ -18,34 +16,39 @@ public:
     virtual ~Op() = default;
   };
 
-  template <typename Subclass, typename... AcceptableTypes> // one per argument
+  template <template <typename...> typename Subclass,
+            typename... AcceptableTypes> // one per argument
   class Operator : public Op {
-    using type_indices = index_sequence_for<AcceptableTypes...>;
+    using type_indices = std::index_sequence_for<AcceptableTypes...>;
 
   public:
     boss::Expression operator()(ExpressionArguments const& args) override {
-      return (*this)(args, index_sequence_for<AcceptableTypes...>{});
+      return (*this)(args, type_indices{});
     }
 
     template <size_t... I>
-    boss::Expression operator()(ExpressionArguments const& args, index_sequence<I...> /*unused*/) {
-      return ((Subclass&)*this)((get<AcceptableTypes>(args.at(I)))...);
+    boss::Expression operator()(ExpressionArguments const& args,
+                                std::index_sequence<I...> /*unused*/) {
+
+      return ((Subclass<AcceptableTypes...>&)*this)((std::get<AcceptableTypes>(args.at(I)))...);
     };
     static auto getSignatures() { return ""; }
     ~Operator() override = default;
   };
 
-  class Plus : public Operator<Plus, int, int> {
+  template <typename... ArgumentTypes> class Plus : public Operator<Plus, ArgumentTypes...> {
   public:
-    auto operator()(int a, int b) { return a + b; }
+    template <typename A, typename B> auto operator()(A a, B b) { return a + b; }
+
+    auto operator()(ArgumentTypes... args) { return (args + ...); }
 
     ~Plus() override = default;
-    ;
   };
 
-  class StringJoin : public Operator<StringJoin, string, string> {
+  template <typename... ArgumentTypes>
+  class StringJoin : public Operator<StringJoin, ArgumentTypes...> {
   public:
-    boss::Expression operator()(string const& a, string const& b) { return a + b; };
+    auto operator()(ArgumentTypes... args) { return (args + ...); }
     ~StringJoin() override = default;
   };
 
@@ -54,30 +57,35 @@ public:
   Engine(Engine&&) = default;
   Engine& operator=(Engine&&) = delete;
   Engine() = default;
-  ;
+
+  class OperatorDirectory : public map<std::string, std::unique_ptr<Op>> {
+  public:
+    template <template <typename...> typename Operator, typename... AcceptedTypes>
+    auto emplace(std::string const& name) {
+      return map::emplace(name, new Operator<AcceptedTypes...>());
+    }
+  };
+
   boss::Expression evaluate(Expression const& e) {
-    map<string, unique_ptr<Op>> operatorDirectory;
-    operatorDirectory.emplace("Plus", new Plus());
-    operatorDirectory.emplace("StringJoin", new StringJoin());
+
+    OperatorDirectory operatorDirectory;
+    operatorDirectory.emplace<Plus, int, int>("Plus");
+    operatorDirectory.emplace<StringJoin, std::string, std::string>("StringJoin");
 
     return std::visit(boss::utilities::overload(
                           [&](boss::ComplexExpression const& e) {
                             auto const& name = e.getHead().getName();
                             auto& op = operatorDirectory.at(name);
                             auto args = e.getArguments();
-                            std::transform(begin(args), end(args), begin(args),
-                                           [this](auto const& unevaluate) {
-                                             return evaluate(unevaluate);
-                                           });
+                            std::transform(
+                                begin(args), end(args), begin(args),
+                                [this](auto const& unevaluate) { return evaluate(unevaluate); });
                             return op->operator()(args);
                           },
-                          [](auto& expression/*unused*/) {
-                            return boss::Expression(expression);
-                          }),
+                          [](auto& expression /*unused*/) { return boss::Expression(expression); }),
                       e);
-    // return {};
   };
-  ~Engine(){};
+  ~Engine() = default;
 };
 
 } // namespace boss::engines::bulk
