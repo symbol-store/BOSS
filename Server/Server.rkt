ad29a3001a59a94f1c6cdfdc057d454225d7c04d
andre <andrea.piermarteri@gmail.com>
2021-09-09 09:17:34 +0000

    example page, example table with numbers, and stub for a visual version of explain

--- Server/Server.rkt
+++ Server/Server.rkt
@@ -105,6 +105,38 @@
                        )
         )))
 
+;K: a very cool new explain function
+(define (visual-explain req operators)
+  (if (equal? (last operators) "RunNativeFunction")
+      (let ((plan #`(~> #,@(unflatten
+                            (map (lambda (op) (read (open-input-string op)))
+                                 operators)))))
+        (embed-in-page '(h1 "Visual Result")
+                       `(pre ,(format "~a" (eval plan)) )
+                       '(hr)
+                       `(pre ,(format "~a" (syntax->datum plan)))
+                       )
+        )
+
+
+      (let ((plan #`(~> #,@(unflatten (map
+                                       (lambda (op) (read (open-input-string op))) operators))))
+            (schema #`(~> #,@(unflatten
+                              (map
+                               (lambda (op) (read (open-input-string op)))
+                               operators))
+                          Schema))
+            )
+
+        (embed-in-page '(h1 "Visual Result")
+                       ; K: THIS gives out data as a string!! 
+                       (format "~v" (eval plan))
+                       ;(list->html-table (eval plan) (eval schema))
+                       ;'(hr)
+                       ;`(pre ,(format "~a" (syntax->datum plan)))
+                       )
+        )))
+
 (define (index req)
   (embed-in-page
    '(h1 "Description")
@@ -119,12 +151,32 @@ all lists (excluding the root), thus stacking another operator on top of the que
    '(list
      (li (a ((href "Customer/:/Project/:/As/Name/FirstName/Last/LastName/Age/age")) "Simple Projection Query"))
      (li (a ((href "Customer/:::/Select/:/Where/:/Equal/FirstName/\"Holger\"/:::/Group/Count")) "Simple Aggregation Query"))
+     ; K: just an example page to play with
+     (li (a ((href "kpage")) "K Page"))
      ))
   )
+
+; K: just an example page to play with
+(define (kpage req)
+  (embed-in-page
+   '(h1 "K Page")
+   "This is K Page!"
+   '(list
+     (li (a ((href "NumbersTable/:/Project/:/As/x/xnumber/y/ynumber/desc/description")) "The Numbers Table"))
+     (li (a ((href "Customer/:/Project/:/As/Name/FirstName/Last/LastName/Age/age")) "The Customer Table"))
+     (li (a ((href "visual/NumbersTable/:/Project/:/As/x/xnumber/y/ynumber/desc/description")) "The Visual Numbers Table"))
+     (li (a ((href "visual/Customer/:/Project/:/As/Name/FirstName/Last/LastName/Age/age")) "The Visual Customer Table"))
+     )
+  )
+)
 
 (define-values (start route-url)
   (dispatch-rules
    [("") index]
+   ; K: just an example page to play with
+   [("kpage") kpage]
+   ; K: visual/something will evaluate as explain, but with visualisation component
+   [("visual" (string-arg) ...) visual-explain]
    [((string-arg) ...) explain]
    ))
 
@@ -135,6 +187,11 @@ all lists (excluding the root), thus stacking another operator on top of the que
      (InsertInto  Customer "Dude" "Englishman" (Interpolate FirstName))
      (InsertInto  Customer "Hubert" "Frenchman" 34)
      (InsertInto  Customer "Andrea" "Italianman" 32)
+     ; K: just an example table to play with, full of numbers
+     (CreateTable NumbersTable xnumber ynumber description)
+     (InsertInto  NumbersTable 1 1 "desc1")
+     (InsertInto  NumbersTable 2 2 "desc2")
+     (InsertInto  NumbersTable 3 3 "desc3")
      ))
  )
 
