b367b1e02dac4370d935a209630b33c8506cd496
Holger <holger@pirk.name>
2021-09-24 11:52:06 +0100

    make linter happy

--- Tests/CAPITests.cpp
+++ Tests/CAPITests.cpp
@@ -1,11 +1,11 @@
+#include <array>
 #include <catch2/catch.hpp>
 
 #include "../Source/BOSS.hpp"
 
 TEST_CASE("Build Expression", "[api]") {
-  auto result = getIntValueFromBOSSExpression(BOSSEvaluate(
-      newComplexBOSSExpression(symbolNameToNewBOSSSymbol("Plus"), 2,
-                               (BOSSExpression**)(BOSSExpression*[]){intToNewBOSSExpression(5),
-                                                                     intToNewBOSSExpression(4)})));
-  CHECK(result == 9);
+  auto result = getIntValueFromBOSSExpression(BOSSEvaluate(newComplexBOSSExpression(
+      symbolNameToNewBOSSSymbol("Plus"), 2,
+      (std::array{intToNewBOSSExpression(3), intToNewBOSSExpression(4)}).data())));
+  CHECK(result == 7);
 }
