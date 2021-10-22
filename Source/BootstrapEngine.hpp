e90dbcfb8251a932d43c7941165a55a60827394e
Holger <holger@pirk.name>
2021-10-22 13:48:43 +0100

    refactor bootstrap engine init to be more compiler-friendly

--- Source/BootstrapEngine.hpp
+++ Source/BootstrapEngine.hpp
@@ -12,40 +12,43 @@ public:
     return std::visit(
         boss::utilities::overload(
             [](boss::ComplexExpression const& e) -> boss::Expression {
-              std::unordered_map<boss::Symbol,
-                                 std::function<boss::Expression(boss::ComplexExpression const&)>>
-                  a{{boss::Symbol("EvaluateInEngine"),
-                     [](auto const& e) {
-                       auto libraryPath = std::get<std::string>(e.getArguments().at(0));
-                       return std::visit(
-                           [libraryPath,
-                            library = dlopen(libraryPath.c_str(), RTLD_LAZY)](auto const& e) {
-                             auto* sym = dlsym(library, "evaluate");
-                             if(sym == nullptr) {
-                               throw std::runtime_error("library \"" + libraryPath +
-                                                        "\" does not provide an evaluate function");
-                             }
-                             auto wrapper = BOSSExpression{.delegate = e};
-                             auto* r = reinterpret_cast<BOSSExpression* (*)(BOSSExpression*)>(sym)(
-                                 &wrapper);
-                             auto result = r->delegate;
-                             free(r); // NOLINT
-                             return result;
-                           },
-                           e.getArguments().at(1));
-                     }},
-                    {boss::Symbol("Plus"), [](auto const& e) {
-                       return std::visit(boss::utilities::overload(
-                                             [e](int a) -> boss::Expression {
-                                               for(auto it = next(e.getArguments().begin());
-                                                   it != e.getArguments().end(); ++it) {
-                                                 a += std::get<decltype(a)>(*it);
-                                               }
-                                               return a;
-                                             },
-                                             [](auto const& e) -> boss::Expression { return e; }),
-                                         e.getArguments().at(0));
-                     }}};
+              static auto const a = [] {
+                auto result =
+                    std::unordered_map<boss::Symbol, std::function<boss::Expression(
+                                                         boss::ComplexExpression const&)>>();
+                result[boss::Symbol("EvaluateInEngine")] = [](auto const& e) {
+                  auto libraryPath = std::get<std::string>(e.getArguments().at(0));
+                  return std::visit(
+                      [libraryPath,
+                       library = dlopen(libraryPath.c_str(), RTLD_LAZY)](auto const& e) {
+                        auto* sym = dlsym(library, "evaluate");
+                        if(sym == nullptr) {
+                          throw std::runtime_error("library \"" + libraryPath +
+                                                   "\" does not provide an evaluate function");
+                        }
+                        auto wrapper = BOSSExpression{.delegate = e};
+                        auto* r =
+                            reinterpret_cast<BOSSExpression* (*)(BOSSExpression*)>(sym)(&wrapper);
+                        auto result = r->delegate;
+                        free(r); // NOLINT
+                        return result;
+                      },
+                      e.getArguments().at(1));
+                };
+                result[boss::Symbol("Plus")] = [](auto const& e) {
+                  return std::visit(boss::utilities::overload(
+                                        [e](int a) -> boss::Expression {
+                                          for(auto it = next(e.getArguments().begin());
+                                              it != e.getArguments().end(); ++it) {
+                                            a += std::get<decltype(a)>(*it);
+                                          }
+                                          return a;
+                                        },
+                                        [](auto const& e) -> boss::Expression { return e; }),
+                                    e.getArguments().at(0));
+                };
+                return std::move(result);
+              }();
               return a.at(e.getHead())(e);
             },
             [](auto& e) -> boss::Expression { return e; }),
