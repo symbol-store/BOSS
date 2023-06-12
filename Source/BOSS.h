1c7fc318fca2c1612223506975ef4b201c4d83dc
Holger Pirk <holger@pirk.name>
2023-06-12 17:49:22 +0100

    factor out the C-bits of the interop

---
+++ Source/BOSS.h
@@ -0,0 +1,48 @@
+#ifndef BOSS_H
+#define BOSS_H
+#ifdef __cplusplus
+extern "C" {
+#include <cinttypes>
+#else
+#include <inttypes.h>
+#include <stdbool.h>
+#endif
+
+struct BOSSSymbol;
+struct BOSSSymbol* symbolNameToNewBOSSSymbol(char const* name);
+char const* symbolToNewString(struct BOSSSymbol const* arg);
+
+struct BOSSExpression;
+struct BOSSExpression* longToNewBOSSExpression(int64_t value);
+struct BOSSExpression* doubleToNewBOSSExpression(double value);
+struct BOSSExpression* stringToNewBOSSExpression(char const* string);
+struct BOSSExpression* symbolNameToNewBOSSExpression(char const* name);
+
+struct BOSSExpression* newComplexBOSSExpression(struct BOSSSymbol* head, size_t cardinality,
+                                                struct BOSSExpression* arguments[]);
+
+/**
+ *     bool = 0, long = 1, double = 2 , std::string = 3, Symbol = 4 , ComplexExpression = 5
+ */
+size_t getBOSSExpressionTypeID(struct BOSSExpression const* arg);
+
+bool getBoolValueFromBOSSExpression(struct BOSSExpression const* arg);
+int64_t getLongValueFromBOSSExpression(struct BOSSExpression const* arg);
+double getDoubleValueFromBOSSExpression(struct BOSSExpression const* arg);
+char* getNewStringValueFromBOSSExpression(struct BOSSExpression const* arg);
+char const* getNewSymbolNameFromBOSSExpression(struct BOSSExpression const* arg);
+
+struct BOSSSymbol* getHeadFromBOSSExpression(struct BOSSExpression const* arg);
+size_t getArgumentCountFromBOSSExpression(struct BOSSExpression const* arg);
+struct BOSSExpression** getArgumentsFromBOSSExpression(struct BOSSExpression const* arg);
+
+struct BOSSExpression* BOSSEvaluate(struct BOSSExpression const* arg);
+void freeBOSSExpression(struct BOSSExpression* expression);
+void freeBOSSArguments(struct BOSSExpression** arguments);
+void freeBOSSSymbol(struct BOSSSymbol* symbol);
+void freeBOSSString(char* string);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOSS_H */
--- Source/BOSS.hpp
+++ Source/BOSS.hpp
@@ -1,46 +1,14 @@
 #pragma once
 #include "Engine.hpp"
 #include "Expression.hpp"
-
-extern "C" {
-struct BOSSSymbol {
-  boss::Symbol delegate;
-};
-BOSSSymbol* symbolNameToNewBOSSSymbol(char const* name);
-char const* symbolToNewString(BOSSSymbol const* arg);
+#include "BOSS.h"
 
 struct BOSSExpression {
   boss::Expression delegate;
 };
-BOSSExpression* longToNewBOSSExpression(int64_t value);
-BOSSExpression* doubleToNewBOSSExpression(double value);
-BOSSExpression* stringToNewBOSSExpression(char const* string);
-BOSSExpression* symbolNameToNewBOSSExpression(char const* name);
-
-BOSSExpression* newComplexBOSSExpression(BOSSSymbol* head, size_t cardinality,
-                                         BOSSExpression* arguments[]);
-
-/**
- *     bool = 0, long = 1, double = 2 , std::string = 3, Symbol = 4 , ComplexExpression = 5
- */
-size_t getBOSSExpressionTypeID(BOSSExpression const* arg);
-
-bool getBoolValueFromBOSSExpression(BOSSExpression const* arg);
-std::int64_t getLongValueFromBOSSExpression(BOSSExpression const* arg);
-std::double_t getDoubleValueFromBOSSExpression(BOSSExpression const* arg);
-char* getNewStringValueFromBOSSExpression(BOSSExpression const* arg);
-char const* getNewSymbolNameFromBOSSExpression(BOSSExpression const* arg);
-
-BOSSSymbol* getHeadFromBOSSExpression(BOSSExpression const* arg);
-size_t getArgumentCountFromBOSSExpression(BOSSExpression const* arg);
-BOSSExpression** getArgumentsFromBOSSExpression(BOSSExpression const* arg);
-
-BOSSExpression* BOSSEvaluate(BOSSExpression const* arg);
-void freeBOSSExpression(BOSSExpression* expression);
-void freeBOSSArguments(BOSSExpression** arguments);
-void freeBOSSSymbol(BOSSSymbol* symbol);
-void freeBOSSString(char* string);
-}
+struct BOSSSymbol {
+  boss::Symbol delegate;
+};
 
 namespace boss {
 expressions::Expression evaluate(expressions::Expression const& expr);
