a756be17757d71eea8086dcc23b5ef0a0904e3a5
David-C-L <david.dave4248@gmail.com>
2024-02-28 23:16:37 +0000

    remove debug

--- Source/BootstrapEngine.hpp
+++ Source/BootstrapEngine.hpp
@@ -6,7 +6,6 @@
 #include "Expression.hpp"
 #include "ExpressionUtilities.hpp"
 #include "Utilities.hpp"
-#include <iostream>
 
 #ifndef _WIN32
 #include <dlfcn.h>
@@ -98,18 +97,12 @@ class BootstrapEngine : public boss::Engine {
 
     void clear() {
       for(const auto& [name, library] : *this) {
-	std::cout << "NAME: " << name << std::endl;
         if(library.resetFunction != nullptr) {
-	  std::cout << "HAS RESET" << std::endl;
           reinterpret_cast<void (*)(void)>(library.resetFunction)();
-	  std::cout << "RESET" << std::endl;
         }
         dlclose(library.library);
-	std::cout << "CLOSED" << std::endl;
       }
-      std::cout << "DONE LOOP" << std::endl;
       ::std::unordered_map<::std::string, LibraryAndFunctions>::clear();
-      std::cout << "DONE CLEAR" << std::endl;
     }
 
     LibraryCache() = default;
