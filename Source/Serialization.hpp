9da2df2d3daf22d642a8fded900505279de06e8f
David-C-L <david.dave4248@gmail.com>
2025-06-24 15:12:21 +0100

    replace libomp condition in getExpressionAsSpanWithIndices to be non-equals

--- Source/Serialization.hpp
+++ Source/Serialization.hpp
@@ -2036,7 +2036,7 @@ public:
 	} else {
 	  std::vector<S> data(currSize);
 
-	  if (n > THREADING_THRESHOLD && currSize > THREADING_THRESHOLD && numSpansOut == 1) {
+	  if (n > THREADING_THRESHOLD && currSize > THREADING_THRESHOLD && numSpansOut != 1) {
 	    #pragma omp parallel for schedule(static) num_threads(20)
 	    for (size_t i = 0; i < currSize; i++) {
 	      const auto& index = indices[i + spanI];
